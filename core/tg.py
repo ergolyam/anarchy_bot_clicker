a7a5e95df777237b0809619050090fdc5affe3b8
Grisha <grisha765@tuta.io>
2025-02-12 02:37:23 +0300

    [u] use automute module

--- core/tg.py
+++ core/tg.py
@@ -4,21 +4,31 @@ from pyrogram.handlers import MessageHandler, EditedMessageHandler
 
 from modes.chasemute import chasemute_func
 from modes.antimute import antimute_func
+from modes.automute import automute_func
 
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 def gen_sessions(mode: str):
     if mode == 'chasemute' and Config.click_users:
-        return Config.click_users
+        users = Config.click_users
+        if Config.automute:
+            users = users + [Config.chase_user + "_automute"]
+        return users
     elif mode == 'antimute' and Config.protect_users:
         return [Config.protect_users[0]]
     elif mode == 'mixed' and Config.protect_users and Config.click_users:
-        return [Config.protect_users[0]] + Config.click_users
+        users = [Config.protect_users[0]] + Config.click_users
+        if Config.automute:
+            users = users + [Config.chase_user + "_automute"]
+        return users
     else:
         return
 
+first_protect_user = False
+
 def create_app(session_name: str, mode: str) -> Client:
+    global first_protect_user
     app = Client(
         name=f"{Config.sessions_path}/{session_name.strip(' ').strip('@')}",
         api_id=Config.tg_id,
@@ -26,22 +36,31 @@ def create_app(session_name: str, mode: str) -> Client:
     )
     if mode == 'chasemute' and Config.chase_user:
         logging.info(f'Apply {mode} handlers.')
-        logging.debug(f'Apply {session_name} as chasemute session.')
-        app.add_handler(MessageHandler(chasemute_func, filters.group))
-        app.add_handler(EditedMessageHandler(chasemute_func, filters.group))
+        if session_name == Config.chase_user and Config.automute:
+            logging.debug(f'Apply {session_name} as automute session.')
+            app.add_handler(EditedMessageHandler(automute_func, filters.group))
+        else:
+            logging.debug(f'Apply {session_name} as chasemute session.')
+            app.add_handler(MessageHandler(chasemute_func, filters.group))
+            app.add_handler(EditedMessageHandler(chasemute_func, filters.group))
     elif mode == 'antimute' and Config.protect_users:
         logging.info(f'Apply {mode} handlers.')
         logging.debug(f'Apply {session_name} as antimute session.')
         app.add_handler(MessageHandler(antimute_func, filters.group))
     elif Config.mode == "mixed":
         logging.info(f'Apply {mode} handlers.')
-        if Config.protect_users and session_name == Config.protect_users[0]:
+        if Config.protect_users and session_name == Config.protect_users[0] and not first_protect_user:
             logging.debug(f'Apply {session_name} as antimute session.')
             app.add_handler(MessageHandler(antimute_func, filters.group))
+            first_protect_user = True
         else:
-            logging.debug(f'Apply {session_name} as chasemute session.')
-            app.add_handler(MessageHandler(chasemute_func, filters.group))
-            app.add_handler(EditedMessageHandler(chasemute_func, filters.group))
+            if session_name == Config.chase_user and Config.automute:
+                logging.debug(f'Apply {session_name} as automute session.')
+                app.add_handler(EditedMessageHandler(automute_func, filters.group))
+            else:
+                logging.debug(f'Apply {session_name} as chasemute session.')
+                app.add_handler(MessageHandler(chasemute_func, filters.group))
+                app.add_handler(EditedMessageHandler(chasemute_func, filters.group))
     else:
         logging.error('Env is not set or config is incomplete.')
     return app
