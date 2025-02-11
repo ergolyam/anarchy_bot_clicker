0b90c50485ec3befe418dbece423df7fffc1c023
Grisha <grisha765@tuta.io>
2025-02-11 22:04:11 +0300

    [u] multiusers

--- core/tg.py
+++ core/tg.py
@@ -1,5 +1,7 @@
+import asyncio
 from config.config import Config
 from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler, EditedMessageHandler
 
 from modes.chasemute import chasemute_func
 from modes.antimute import antimute_func
@@ -7,10 +9,8 @@ from modes.antimute import antimute_func
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-app = Client(f"{Config.sessions_path}/{Config.usermain.strip('@')}", api_id=Config.tg_id, api_hash=Config.tg_hash)
+USER_SESSIONS = Config.usersmain or [Config.usernames[0]]
 
-@app.on_message(filters.group)
-@app.on_edited_message(filters.group)
 async def handle_messages(client, message):
     mode = Config.mode
     if mode == 'chasemute':
@@ -18,7 +18,7 @@ async def handle_messages(client, message):
             logging.error('Env userchase is not set')
             return
         await chasemute_func(client, message)
-    if mode == 'antimute':
+    elif mode == 'antimute':
         if not Config.usernames:
             logging.error('Env usernames is not set')
             return
@@ -27,13 +27,32 @@ async def handle_messages(client, message):
         logging.error('Env mode is not set')
         return
 
+def create_app(session_name: str) -> Client:
+    app = Client(
+        name=f"{Config.sessions_path}/{session_name.strip(' ').strip('@')}",
+        api_id=Config.tg_id,
+        api_hash=Config.tg_hash
+    )
+    app.add_handler(MessageHandler(handle_messages, filters.group))
+    app.add_handler(EditedMessageHandler(handle_messages, filters.group))
+    
+    return app
+
+clients = [create_app(sess) for sess in USER_SESSIONS]
+
 async def start_bot():
-    logging.info("Launching the bot...")
-    await app.start()
+    logging.info("Launching all clients...")
+    for client in clients:
+        logging.info(f"Starting client: {client.name}")
+        await client.start()
+    logging.info("All clients have been started.")
 
 async def stop_bot():
-    logging.info("Stopping the bot...")
-    await app.stop()
+    logging.info("Stopping all clients...")
+    for client in clients:
+        logging.info(f"Stopping client: {client.name}")
+        await client.stop()
+    logging.info("All clients have been stopped.")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
